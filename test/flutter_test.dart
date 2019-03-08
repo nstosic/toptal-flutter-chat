c21a340f473b50331ee169ddcb92b7420790defc
nstosic <nemanjastosic@outlook.com>
2019-03-08 13:04:21 +0100

    Removed outdated tests

--- test/flutter_test.dart
+++ test/flutter_test.dart
@@ -15,14 +15,6 @@ import 'package:toptal_chat/util/util.dart';
 
 
 void main() {
-  group("CreateChatroomState", () {
-    test(".cancel(CreateChatroomState) factory sets optional CreateChatroomAction parameter", () {
-      CreateChatroomState state = CreateChatroomState.initial();
-      CreateChatroomState newState = CreateChatroomState.cancel(state);
-      expect(newState.action.chatroom, null);
-      expect(newState.action.canceled, true);
-    });
-  });
   group("Util", () {
     test('.swapElementsInList() swaps correct elements', () {
       List<int> intList = List.from([1, 2, 3, 4]);
