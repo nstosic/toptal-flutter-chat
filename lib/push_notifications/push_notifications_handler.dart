479e0e85fdf266424fde63e99cde3ab75bf9365d
nstosic <nemanjastosic@outlook.com>
2019-04-05 15:41:14 +0200

    Implemented FirebaseMessaging handlers for incoming notifications

--- lib/push_notifications/push_notifications_handler.dart
+++ lib/push_notifications/push_notifications_handler.dart
@@ -1,9 +1,17 @@
 import 'dart:io';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
 
 import '../model/user_repo.dart';
+import '../model/chat_repo.dart';
+import '../model/user.dart';
+import '../navigation_helper.dart';
 
 class PushNotificationsHandler {
+  BuildContext context;
+
+  PushNotificationsHandler(this.context);
+
   FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
 
   void setup() {
@@ -13,7 +21,18 @@ class PushNotificationsHandler {
       _firebaseMessaging.getToken().then((token) => UserRepo.getInstance().setFCMToken(token));
     }
 
-
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) {
+        print("Incoming notification message:");
+        print(message);
+      },
+      onResume: (Map<String, dynamic> message) {
+        _handleIncomingNotification(message);
+      },
+      onLaunch: (Map<String, dynamic> message) {
+        _handleIncomingNotification(message);
+      }
+    );
   }
 
   void _requestPermissionOniOS() {
@@ -23,5 +42,19 @@ class PushNotificationsHandler {
         _firebaseMessaging.getToken().then((token) => UserRepo.getInstance().setFCMToken(token));
       }
     });
+  }
+
+  void _handleIncomingNotification(Map<String, dynamic> payload) async {
+    Map<String, dynamic> userMap = payload["other_member_id"];
+    User otherUser = User(userMap["uid"], userMap["displayName"], userMap["photoUrl"], userMap["fcmToken"]);
+    User currentUser = await UserRepo.getInstance().getCurrentUser();
+    if (currentUser == null) {
+      return;
+    }
+    ChatRepo.getInstance()
+        .getChatroom(payload["tag"], currentUser, otherUser)
+        .then((chatroom) {
+          NavigationHelper.navigateToInstantMessaging(context, otherUser.displayName, chatroom.id);
+        });
   }
 }
